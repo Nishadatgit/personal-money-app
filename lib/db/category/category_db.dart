76525383fa84959c250a0385c590b3ddd1a0e424
Nishadatgit <nishadns321@gmail.com>
2022-06-04 14:40:48 +0530

    generated hive fields

---
+++ lib/db/category/category_db.dart
@@ -0,0 +1,14 @@
+import 'package:moneymanagement/models/category/category_model.dart';
+
+abstract class CategoryDbFunctions {
+  //List<CategoryModel> getCategories();
+  Future<void> insertCategory(CategoryModel value);
+}
+
+class CategoryDb implements CategoryDbFunctions{
+  @override
+  Future<void> insertCategory(CategoryModel value) async{
+    
+    
+  }
+}
