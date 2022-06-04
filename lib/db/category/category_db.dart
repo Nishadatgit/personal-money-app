3d4daf1aa872d4363a41faa227f188e0d884ee1a
Nishadatgit <nishadns321@gmail.com>
2022-06-04 15:00:47 +0530

    added category insert and get functions

--- lib/db/category/category_db.dart
+++ lib/db/category/category_db.dart
@@ -1,14 +1,23 @@
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:moneymanagement/models/category/category_model.dart';
 
+const CATEGORY_DB_NAME = 'category_database';
+
 abstract class CategoryDbFunctions {
-  //List<CategoryModel> getCategories();
+  Future<List<CategoryModel>> getCategories();
   Future<void> insertCategory(CategoryModel value);
 }
 
-class CategoryDb implements CategoryDbFunctions{
+class CategoryDb implements CategoryDbFunctions {
   @override
-  Future<void> insertCategory(CategoryModel value) async{
-    
-    
+  Future<void> insertCategory(CategoryModel value) async {
+    final _category_DB = await Hive.openBox<CategoryModel>(CATEGORY_DB_NAME);
+    await _category_DB.add(value);
+  }
+
+  @override
+  Future<List<CategoryModel>> getCategories() async {
+    final _category_DB = await Hive.openBox<CategoryModel>(CATEGORY_DB_NAME);
+    return _category_DB.values.toList();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,30 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:moneymanagement/models/category/category_model.dart';
 import 'package:moneymanagement/screens/home/screen_home.dart';
 
-void main(List<String> args) {
-  runApp(MyApp());
+Future<void> main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
+  if (!Hive.isAdapterRegistered(CategoryTypeAdapter().typeId)) {
+    Hive.registerAdapter(CategoryTypeAdapter());
+  }
+
+  if (!Hive.isAdapterRegistered(CategoryModelAdapter().typeId)) {
+    Hive.registerAdapter(CategoryModelAdapter());
+  }
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.pink
-      ),
-      home:const ScreenHome(),
+      theme: ThemeData(primarySwatch: Colors.pink),
+      home: const ScreenHome(),
     );
   }
 }
